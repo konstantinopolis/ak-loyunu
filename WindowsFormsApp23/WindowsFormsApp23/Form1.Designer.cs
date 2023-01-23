3cc14ab99a0227543044439a66c16362bba9badc
Zehra <121049907+konstantinopolis@users.noreply.github.com>
2023-01-23 05:18:08 +0300

    Add files via upload

---
+++ WindowsFormsApp23/WindowsFormsApp23/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsFormsApp23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(1, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(385, 385);
+            this.tableLayoutPanel1.TabIndex = 1;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(386, 384);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zeka Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp23/WindowsFormsApp23/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Diagnostics;
+
+namespace WindowsFormsApp23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] Elemanlar = { "hero1", "hero2", "hero3", "hero4", "hero5", "hero6", "hero7", "hero8", "hero1", "hero2","hero3","hero4","hero5","hero6 ","hero7","hero8" };
+        ArrayList liste1 = new ArrayList();
+        ArrayList liste2 = new ArrayList();
+        ArrayList secimler = new ArrayList(2); //secimler icin dizi
+        PictureBox[] resimler = new PictureBox[16];//resimler icin dizi
+        int acilan_resim = 0;
+        private void Resim_Ac(string isim, int i)
+        {   //isimlere göre resim atamasi yapılır
+            if (isim == "hero1")
+            {
+                resimler[i].Image = Properties.Resources.hero1;
+            }
+            else if (isim == "hero2")
+            {
+                resimler[i].Image = Properties.Resources.hero2;
+            }
+            else if (isim == "hero3")
+            {
+                resimler[i].Image = Properties.Resources.hero3;
+            }
+            else if (isim == "hero4")
+            {
+                resimler[i].Image = Properties.Resources.hero4;
+            }
+            else if (isim == "hero5")
+            {
+                resimler[i].Image = Properties.Resources.hero5;
+            }
+            else if (isim == "hero6")
+            {
+                resimler[i].Image = Properties.Resources.hero6;
+            }
+            else if (isim == "hero7")
+            {
+                resimler[i].Image = Properties.Resources.hero7;
+            }
+            else if (isim == "hero8")
+            {
+                resimler[i].Image = Properties.Resources.hero8;
+            }
+        }
+
+        private void Oyun_Baslat()
+        {
+
+            liste1 = new ArrayList();//listeler sıfırlanır
+            liste2 = new ArrayList();
+            resimler = new PictureBox[16];
+
+            Random rnd = new Random();
+            foreach (string i in Elemanlar) //liste1 icine üyeler eklenir
+            {
+                liste1.Add(i);
+            }
+
+            for (int i = 0; i < 16; i++)
+            {
+                int index = rnd.Next(0, liste1.Count); //liste1 elemanlarindan birinin indexini seçer rastgele
+                liste2.Add(liste1[index]); //bu indexe ait liste1 elemanini liste2ye ekler
+                liste1.RemoveAt(index);//liste1deki elemani kaldirir
+            } //elemanlar listesi rastgele sekilde liste2ye dagitilmis olur
+
+            for (int i = 0; i < 16; i++)
+            {
+                resimler[i] = new PictureBox(); //resim olusturur
+                resimler[i].Name = i.ToString(); //ismini olusturur
+                resimler[i].Size = new Size(90, 90); //boyutunu olusturur
+                resimler[i].Image = Properties.Resources.which_is_hero; //baslangic resmini belirler
+                resimler[i].SizeMode = PictureBoxSizeMode.Zoom;
+                resimler[i].Click += Resimler_Click; //tiklama özelligi kazandirir
+                tableLayoutPanel1.Controls.Add(resimler[i]);
+            }
+        }
+        
+        private async void Resimler_Click(object sender, EventArgs e)
+        {
+
+            PictureBox p = (PictureBox)sender; 
+            int index=Convert.ToInt32(p.Name.ToString());//resmin indexini yollar
+
+            if (secimler.Count != 2) //eger iki tane secim yapilmamissa
+            {
+                
+                secimler.Add(index); //sceim yaparz
+                Resim_Ac(liste2[index].ToString(), index); //liste2deki sayiya göre resim acar
+                resimler[index].Enabled = false; //resimin tekrar secilmesi engellenir
+            }
+            if(secimler.Count==2) //eger iki tane secildiyse
+            {
+                if(liste2[Convert.ToInt32(secimler[0])]==liste2[Convert.ToInt32(secimler[1])])
+                {
+                    acilan_resim += 2;//acilan resmi 2 artirir
+                    secimler = new ArrayList(); //secimleri sifirlar
+                    if(acilan_resim==16) //eger 16 tane resim acildiysa
+                    {
+                        MessageBox.Show(skor); //skoru ekrana yollar
+                        timer1.Enabled = false; // zamani durdurur
+                        Application.Restart(); //uygulamayi baslatir
+                    }
+                }
+                else //resimler es degilse
+                {
+
+                    System.Threading.Thread.Sleep(300);
+                    resimler[Convert.ToInt32(secimler[0])].Image = Properties.Resources.which_is_hero; 
+                    resimler[Convert.ToInt32(secimler[1])].Image = Properties.Resources.which_is_hero;
+                    resimler[Convert.ToInt32(secimler[0])].Enabled = true;//resimler tekrar aktif olur
+                    resimler[Convert.ToInt32(secimler[1])].Enabled = true;
+                    secimler = new ArrayList(); // secimleri sifirlar
+                }
+            }
+            
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Wtch.Start(); //sayaci baslatir
+            timer1.Enabled = true; //timer baslatir
+            Oyun_Baslat();// oyun baslatir
+           
+        }
+        Stopwatch Wtch = new Stopwatch();
+        public string skor = "";
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            
+                TimeSpan zaman = TimeSpan.FromMilliseconds(Wtch.ElapsedMilliseconds); //milisaniyeyi saniyeye cevirir 
+                 skor = string.Format("{0:D2} hour {1:D2} minute {2:D2} second yaptınız.",
+                            zaman.Hours,
+                            zaman.Minutes,       //saat dakika saniye formatina dönüstürürz
+                            zaman.Seconds,
+                            zaman.Milliseconds);
+                
+            }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+    
+}
